a61eb395c58f27388345b3896a4efa71299a1da4
PackChanHyo <cksgy1510@gmail.com>
2023-04-04 15:39:39 +0900

    04-04_1문제

---
+++ 1레벨/소수찾기.js
@@ -0,0 +1,27 @@
+// 문제 설명
+// 1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+
+// 소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
+// (1은 소수가 아닙니다.)
+
+// 입출력 예
+// n	result
+// 10	4
+// 5	3
+
+// 나의 풀이
+
+function solution(n) {
+  var answer = [];
+  let arr = Array(n + 1)
+    .fill(true)
+    .fill(false, 0, 2);
+  for (let i = 2; i * i < n; i++) {
+    if (arr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
+      }
+    }
+  }
+  return arr.filter((e) => e).length;
+}
